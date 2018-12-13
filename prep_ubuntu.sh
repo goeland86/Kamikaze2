94debcd1c616d2db8c626b3f1133403240d66640
Andrew Wiley <wiley@outlook.com>
2018-12-13 00:41:06 -0800

    disable the power button

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -52,6 +52,9 @@ upgrade_base_operating_system() {
   apt-get -y -q --no-install-recommends --force-yes install unzip iptables iptables-persistent
   systemctl enable netfilter-persistent
   sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+  # disable the power button
+  echo "HandlePowerKey=ignore" >> /etc/systemd/logind.conf
 }
 
 add_thing_printer_support_repository() {
