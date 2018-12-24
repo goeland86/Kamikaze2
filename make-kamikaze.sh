3ec14d3e98ed94502d9d8c6487c21032ffcb9a79
Richard Wackerbarth <rkw@dataplex.net>
2018-12-24 12:04:01 -0600

    Clean up sudo configuration

--- make-kamikaze.sh
+++ make-kamikaze.sh
@@ -38,7 +38,7 @@ export PATH=`pwd`/Packages:$PATH
 install_sgx
 setup_port_forwarding
 install_dependencies
-create_user
+create_octoprint_user
 install_service_virtualization
 install_redeem
 install_octoprint
