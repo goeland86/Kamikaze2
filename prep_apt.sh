94d79e24c8cdb4d10968c901ea8ef2ec940d2326
Elias Bakken <elias@iagent.no>
2021-01-21 12:45:20 +0100

    Install setuptools

--- prep_apt.sh
+++ prep_apt.sh
@@ -8,6 +8,7 @@ echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
 #apt-get -y install ansible
 apt install -y python3-pip
+pip3 install setuptools
 pip3 install ansible
 
 #sed -i 's/gather_timeout=10/gather_timeout=20/g' /etc/ansible/ansible.cfg
