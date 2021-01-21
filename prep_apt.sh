3386fd8f03e2b83d91fe5e76fc578685a9d65fa4
Elias Bakken <elias@iagent.no>
2021-01-21 13:59:34 +0100

    Python is needed

--- prep_apt.sh
+++ prep_apt.sh
@@ -6,12 +6,8 @@ echo "Installing ansible"
 chmod o+r /etc/resolv.conf
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
-#apt-get -y install ansible
-apt install -y python3-pip
-pip3 install setuptools
-pip3 install wheel
-pip3 install ansible
+apt install -y ansible python
 
-#sed -i 's/gather_timeout=10/gather_timeout=20/g' /etc/ansible/ansible.cfg
+sed -i 's/gather_timeout=10/gather_timeout=20/g' /etc/ansible/ansible.cfg
 
 echo "Now the system is ready to use the ansible playbooks to build images."
